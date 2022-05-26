b1c02f84a0dbb8934962c1f4cebdefc907bf449a
mncdg <mncdg@ukr.net>
2022-05-26 11:18:19 +0300

    Fix logging of response

--- debridge_node/src/modules/external/common/addHttpServiceLogging.ts
+++ debridge_node/src/modules/external/common/addHttpServiceLogging.ts
@@ -3,11 +3,27 @@ import { Logger } from '@nestjs/common';
 
 export function addHttpServiceLogging(httpService: HttpService, logger: Logger) {
   httpService.axiosRef.interceptors.request.use(
-    request => logger.log(`Http request ${JSON.stringify(request)}`),
-    request => logger.error(`Http request ${JSON.stringify(request.message)}`),
+    request => {
+      logger.log(`Http request ${JSON.stringify(request)}`);
+      return request;
+    },
+    request => {
+      logger.error(`Http request ${JSON.stringify(request.message)}`);
+      return Promise.reject(request);
+    },
   );
   httpService.axiosRef.interceptors.response.use(
-    response => logger.log(`Http response ${JSON.stringify(response)}`),
-    response => logger.error(`Http response ${response.message}`),
+    response => {
+      logger.log(
+        `Http response status: ${response.status} statusText: ${response.statusText} headers: ${JSON.stringify(
+          response.headers,
+        )} data: ${JSON.stringify(response.data)}`,
+      );
+      return response;
+    },
+    response => {
+      logger.error(`Http response ${JSON.stringify(response)}`);
+      return Promise.reject(response);
+    },
   );
 }
