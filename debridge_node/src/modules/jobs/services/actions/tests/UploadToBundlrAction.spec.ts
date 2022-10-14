0208568ed58a80d402aa5a666f22ca602733f70a
mncdg <kam123ua@gmail.com>
2022-10-14 13:10:45 +0300

    Fix tests

--- debridge_node/src/modules/jobs/services/actions/tests/UploadToBundlrAction.spec.ts
+++ debridge_node/src/modules/jobs/services/actions/tests/UploadToBundlrAction.spec.ts
@@ -4,15 +4,25 @@ import { HttpModule } from '@nestjs/axios';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { SubmissionEntity } from '../../../../../entities/SubmissionEntity';
 import { ConfirmNewAssetEntity } from '../../../../../entities/ConfirmNewAssetEntity';
-import { In, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { UploadStatusEnum } from '../../../../../enums/UploadStatusEnum';
 import { Web3Service } from '../../../../web3/services/Web3Service';
-import { chainConfigJsonMock } from '../../../../../tests/mocks/chain.config.json.mock';
 import { UploadToBundlrAction } from '../UploadToBundlrAction';
 import { BundlrService } from '../../../../external/bundlr/bundlr.service';
 
 jest.mock('../../../../../config/chains_config.json', () => {
-  return chainConfigJsonMock;
+  return [
+    {
+      chainId: 970,
+      name: 'ETHEREUM',
+      debridgeAddr: '0x43dE2d77BF8027e25dBD179B491e8d64f38398aA',
+      firstStartBlock: 13665321,
+      provider: 'https://debridge.io',
+      interval: 10000,
+      blockConfirmation: 12,
+      maxBlockRange: 5000,
+    },
+  ];
 });
 
 describe('UploadToBundlrAction', () => {
@@ -96,7 +106,7 @@ describe('UploadToBundlrAction', () => {
     await service.process();
     expect(submissionRepository.update).toHaveBeenCalledWith(
       {
-        submissionId: In(['123']),
+        submissionId: '123',
       },
       {
         bundlrStatus: UploadStatusEnum.UPLOADED,
@@ -110,7 +120,7 @@ describe('UploadToBundlrAction', () => {
     await service.process();
     expect(confirmNewAssetRepository.update).toHaveBeenCalledWith(
       {
-        deployId: In(['1']),
+        deployId: '1',
       },
       {
         bundlrStatus: UploadStatusEnum.UPLOADED,
