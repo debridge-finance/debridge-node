570010fb1b0a89e33885a43eda298df9e81f74ce
mncdg <mncdg@ukr.net>
2023-05-23 02:44:53 +0200

    feature: new solana reader

--- debridge_node/src/modules/chain/scanning/services/SolanaReaderService.ts
+++ debridge_node/src/modules/chain/scanning/services/SolanaReaderService.ts
@@ -38,9 +38,6 @@ export class SolanaReaderService implements OnModuleInit {
   }
 
   async syncTransactions() {
-    if (!this.#duplex) {
-      await this.createSubscription();
-    }
     const chain = await this.supportedChainRepository.findOne({
       where: {
         chainId: solanaChainId,
@@ -51,10 +48,11 @@ export class SolanaReaderService implements OnModuleInit {
       this.#logger.error(message);
       return;
     }
-    const submissions = [...this.#submissionsFromSync].filter(s => s.nonce > chain.latestNonce);
+    const submissions = [...this.#submissionsFromSync].filter(s => s.nonce >= chain.latestNonce);
     submissions.sort((a, b) => a.nonce - b.nonce);
     this.#submissionsFromSync = [];
     const size = Math.ceil(submissions.length / this.#PAGE_SIZE);
+    //pagination
     for (let pageNumber = 0; pageNumber < size; pageNumber++) {
       const skip = pageNumber * this.#PAGE_SIZE;
       const end = Math.min((pageNumber + 1) * this.#PAGE_SIZE, submissions.length);
@@ -62,15 +60,15 @@ export class SolanaReaderService implements OnModuleInit {
       const lastSubmission = submissionsForProcessing.at(-1);
       const processingResult = await this.chainProcessingService.process(submissionsForProcessing, solanaChainId, lastSubmission.nonce);
       if (processingResult === ProcessNewTransferResultStatusEnum.ERROR) {
-        await this.#duplex?.requests?.complete();
         this.#submissionsFromSync = [];
-        await this.createSubscription();
+        this.createSubscription();
         break;
       }
     }
   }
 
   private async createSubscription() {
+    await this.#duplex?.requests?.complete();
     this.#submissionsFromSync = [];
     const chain = await this.supportedChainRepository.findOne({
       where: {
@@ -82,13 +80,7 @@ export class SolanaReaderService implements OnModuleInit {
       this.#logger.error(message);
       return;
     }
-    let latestNonce;
-    if (chain?.latestNonce) {
-      latestNonce = chain?.latestNonce;
-    } else {
-      latestNonce = 0;
-    }
-    this.#duplex = this.#solanaGrpcClient.getSendEvents(BigInt(latestNonce), true);
+    this.#duplex = this.#solanaGrpcClient.getSendEvents(BigInt(chain?.latestNonce || 0), true);
 
     for await (const response of this.#duplex.responses) {
       if (!(response.sendEventMessage !== undefined && response.sendEventMessage.oneofKind !== undefined)) {
@@ -101,17 +93,12 @@ export class SolanaReaderService implements OnModuleInit {
         case 'heartbeat': {
           // @ts-ignore
           this.#logger.verbose(`Heartbeat: ${JSON.stringify(response.sendEventMessage.heartbeat)}`);
-          clearTimeout(this.#heartbeatIntervalInstance);
-          this.#heartbeatIntervalInstance = setTimeout(() => {
-            this.createSubscription();
-          }, this.configService.get<number>('DEBRIDGE_EVENTS_CONSISTENCY_CHECK_TIMEOUT_SECS', 10) * 10 * 1000);
+
+          this.heartbeat();
           break;
         }
         case 'event': {
-          clearTimeout(this.#heartbeatIntervalInstance);
-          this.#heartbeatIntervalInstance = setTimeout(() => {
-            this.createSubscription();
-          }, this.configService.get<number>('DEBRIDGE_EVENTS_CONSISTENCY_CHECK_TIMEOUT_SECS', 10) * 10 * 1000);
+          this.heartbeat();
           // @ts-ignore
           const event = response.sendEventMessage.event;
           const nonce = Number(U256Converter.toBigInt(event.submission?.nonce).toString());
@@ -125,7 +112,16 @@ export class SolanaReaderService implements OnModuleInit {
     }
   }
 
-  async onModuleInit() {
+  private heartbeat() {
+    clearTimeout(this.#heartbeatIntervalInstance);
+    const timeout = this.configService.get<number>('DEBRIDGE_EVENTS_CONSISTENCY_CHECK_TIMEOUT_SECS') * 10 * 1000;
+    this.#heartbeatIntervalInstance = setTimeout(async () => {
+      this.createSubscription();
+      this.#logger.error(`Duplex is not active for ${timeout}s`);
+    }, timeout);
+  }
+
+  onModuleInit() {
     this.createSubscription();
   }
 }
