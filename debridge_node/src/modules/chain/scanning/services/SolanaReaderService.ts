2b326dc2b7f54bfb5b06f1b7735e9c34c8a06273
mncdg <mncdg@ukr.net>
2023-06-22 13:33:18 +0200

    fix: duplex reconnection

--- debridge_node/src/modules/chain/scanning/services/SolanaReaderService.ts
+++ debridge_node/src/modules/chain/scanning/services/SolanaReaderService.ts
@@ -24,6 +24,7 @@ export class SolanaReaderService implements OnModuleInit {
   #submissionsFromSync: SubmissionEntity[] = [];
   #PAGE_SIZE = 100;
   #heartbeatIntervalInstance: NodeJS.Timeout;
+  #abortController = new AbortController();
 
   constructor(
     @InjectRepository(SupportedChainEntity)
@@ -69,7 +70,11 @@ export class SolanaReaderService implements OnModuleInit {
   }
 
   private async createSubscription() {
-    await this.#duplex?.requests?.complete();
+    if (this.#duplex) {
+      this.#abortController.abort();
+      this.#abortController = new AbortController();
+      this.#solanaGrpcClient.abortSignal = this.#abortController.signal;
+    }
     this.#submissionsFromSync = [];
     const chain = await this.supportedChainRepository.findOne({
       where: {
@@ -83,35 +88,41 @@ export class SolanaReaderService implements OnModuleInit {
     }
     this.#duplex = this.#solanaGrpcClient.getSendEvents(BigInt(chain?.latestNonce || 0), true);
 
-    for await (const response of this.#duplex.responses) {
-      if (response.sendEventMessage?.oneofKind == undefined) {
-        continue;
-      }
-      switch (response.sendEventMessage.oneofKind) {
-        case undefined: {
+    try {
+      for await (const response of this.#duplex.responses) {
+        if (response.sendEventMessage?.oneofKind == undefined) {
           continue;
         }
-        case 'heartbeat': {
-          // @ts-ignore
-          this.#logger.verbose(`Heartbeat: ${JSON.stringify(response.sendEventMessage.heartbeat)}`);
+        switch (response.sendEventMessage.oneofKind) {
+          case undefined: {
+            continue;
+          }
+          case 'heartbeat': {
+            // @ts-ignore
+            this.#logger.verbose(`Heartbeat: ${JSON.stringify(response.sendEventMessage.heartbeat)}`);
 
-          this.heartbeat();
-          // @ts-ignore
-          this.updateLastSyncedBlock(response.sendEventMessage.heartbeat as unknown as SolanaHearbeat);
-          break;
-        }
-        case 'event': {
-          this.heartbeat();
-          // @ts-ignore
-          const event = response.sendEventMessage.event;
-          const nonce = Number(U256Converter.toBigInt(event.submission?.nonce).toString());
-          this.#logger.log(`new event is received nonce: ${nonce}`);
+            this.heartbeat();
+            // @ts-ignore
+            this.updateLastSyncedBlock(response.sendEventMessage.heartbeat as SolanaHearbeat);
+            break;
+          }
+          case 'event': {
+            this.heartbeat();
+            // @ts-ignore
+            const event = response.sendEventMessage.event;
+            const nonce = Number(U256Converter.toBigInt(event.submission?.nonce).toString());
+            this.#logger.log(`new event is received nonce: ${nonce}`);
 
-          const submission = this.transformService.generateSubmissionFromSolanaSendEvent(event);
-          this.#submissionsFromSync.push(submission);
-          break;
+            const submission = this.transformService.generateSubmissionFromSolanaSendEvent(event);
+            this.#submissionsFromSync.push(submission);
+            break;
+          }
         }
       }
+    } catch (e) {
+      const error = e as Record<string, unknown>;
+      if ('code' in error && error.code === 'CANCELLED') this.#logger.error('closed duplexes');
+      else this.#logger.error(e);
     }
   }
 
